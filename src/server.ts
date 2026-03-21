bfab47da4e79fb88265d9155df9f9678233dd5a5
boy397 <aryamukherjee397@outlook.com>
2026-03-22 00:54:16 +0530

    fix

--- src/server.ts
+++ src/server.ts
@@ -39,7 +39,7 @@ app.use(session({
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
         maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days
-        sameSite: 'lax'
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
     }
 }));
 
